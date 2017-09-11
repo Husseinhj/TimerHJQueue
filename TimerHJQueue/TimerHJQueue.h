4658359305a880a7a5669490e66b6fd29cbcf308
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2017-09-12 00:49:57 +0430

    Initial Commit

---
+++ TimerHJQueue/TimerHJQueue.h
@@ -0,0 +1,19 @@
+//
+//  TimerHJQueue.h
+//  TimerHJQueue
+//
+//  Created by Hussein Habibi on 6/21/1396 AP.
+//  Copyright © 1396 AP Hussein.Juybari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TimerHJQueue.
+FOUNDATION_EXPORT double TimerHJQueueVersionNumber;
+
+//! Project version string for TimerHJQueue.
+FOUNDATION_EXPORT const unsigned char TimerHJQueueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimerHJQueue/PublicHeader.h>
+
+
